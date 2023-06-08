94916c6b3bfc1dbc420401f6233abd539284882b
2001kevin <silahisabungan7@gmail.com>
2023-06-08 15:50:53 +0800

    add function

---
+++ assets/js/gallery.js
@@ -0,0 +1,38 @@
+$('.gallery').slick({
+                infinite: true,
+                speed: 1000,
+                autoplay: true,
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: true,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 320,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
